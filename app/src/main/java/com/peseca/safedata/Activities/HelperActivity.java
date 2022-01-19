cc85a55d14b738d69367b50a0556cf1f010fc3b4
Detherminal <c.mertozkan@gmail.com>
2022-01-19 21:55:23 +0300

    Added NewVersionActivity

--- app/src/main/java/com/peseca/safedata/Activities/HelperActivity.java
+++ app/src/main/java/com/peseca/safedata/Activities/HelperActivity.java
@@ -9,16 +9,26 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
 
+import com.peseca.safedata.NewVersionActivity;
 import com.peseca.safedata.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class HelperActivity extends AppCompatActivity {
 
     public static final String SHARED_PREFS = "sharedPrefs";
+    public boolean isNewerVersionAvailable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helper);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        isNewerVersionAvailable = sharedPreferences.getBoolean("isNewerVersionAvailable", false);
+        boolean isVaultCreated = sharedPreferences.getBoolean("isVaultCreated", false);
 
         int screenSize = getResources().getConfiguration().screenLayout &
                 Configuration.SCREENLAYOUT_SIZE_MASK;
@@ -35,20 +45,46 @@ public class HelperActivity extends AppCompatActivity {
                 break;
         }
 
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        boolean isVaultCreated = sharedPreferences.getBoolean("isVaultCreated", false);
-
         Handler delayer = new Handler();
         delayer.postDelayed(new Runnable() {
             public void run() {
-                if (isVaultCreated) {
-                    Intent goToMainActivity = new Intent(HelperActivity.this, MainActivity.class);
-                    startActivity(goToMainActivity);
+                if (isNewerVersionAvailable) {
+                    goToNewVersionActivity();
                 } else {
-                    Intent goToVaultCreation = new Intent(HelperActivity.this, VaultCreationActivity.class);
-                    startActivity(goToVaultCreation);
+                    isThereANewVersion();
+                    if (isNewerVersionAvailable) {
+                        goToNewVersionActivity();
+                    } else {
+                        if (isVaultCreated) {
+                            Intent goToMainActivity = new Intent(HelperActivity.this, MainActivity.class);
+                            startActivity(goToMainActivity);
+                        } else {
+                            Intent goToVaultCreation = new Intent(HelperActivity.this, VaultCreationActivity.class);
+                            startActivity(goToVaultCreation);
+                        }
+                    }
                 }
             }
         }, 1000);
+    }
+
+    public void isThereANewVersion() {
+        String currentMonth = (new SimpleDateFormat("MM", Locale.getDefault()).format(new Date()));
+        String currentYear = (new SimpleDateFormat("yyyy", Locale.getDefault()).format(new Date()));
+        int currentYearAndMonth = Integer.parseInt(currentYear+currentMonth);
+        if (currentYearAndMonth > 202208){
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isNewerVersionAvailable", true);
+            editor.apply();
+            isNewerVersionAvailable = true;
+        } else {
+            isNewerVersionAvailable = false;
+        }
+    }
+
+    public void goToNewVersionActivity() {
+        Intent goToNewVersionActivity = new Intent(this, NewVersionActivity.class);
+        startActivity(goToNewVersionActivity);
     }
 }
